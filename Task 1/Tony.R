065f03ab401b4a8e0e6e8200271758adbe78ff75
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2022-02-05 09:23:01 +0700

    Update Tony.R

--- Task 1/Tony.R
+++ Task 1/Tony.R
@@ -1,17 +1,35 @@
 library(shiny)
 
-help("renderTable")
 ui <- fluidPage(
-  titlePanel(h3("Tony Alfan")),
+  titlePanel("Tony Alfan", windowTitle = "Task 1 Tony"),
   
   sidebarLayout(
     sidebarPanel(
+      
+      #Buat input berupa file dengan id "file"
       fileInput(inputId = "file",
                 label = "Input Data",
                 placeholder = "Masukkan Data"),
+      
+      #Buat Input warna
+      textInput(inputId = "colour",
+                label = "Masukkan kode Hex warna",
+                placeholder = "Contoh : #30D5C8"),
+      
+      
+      #Buat Checkbox untuk memilih apakah akan menggunakan 
       checkboxInput(inputId = "chk",
-                    label = "Baris Pertama Kolom",
-                    value = F)
+                    label = "Baris Pertama Nama Kolom",
+                    value = F),
+      
+      #Buat Checkbox untuk order val
+      checkboxInput(inputId = "order",
+                    label = "Urutkan Kategori"),
+      
+      #Buat Checkbox untuk reverse
+      checkboxInput(inputId = "rev",
+                    label = "Balikkan Urutan Data")
+      
     ),
     
     mainPanel(
@@ -24,5 +42,36 @@ ui <- fluidPage(
 
 
 server <- function(input,output, session) {
-  output$table <- renderTable(input$file)
+  data <- reactive({
+    req(input$file)
+    file1 <- input$file
+    if(is.null(file1)){return()}
+    read.csv(file = file1$datapath, header = input$chk)
+  })
+  
+  tab <-  reactive({
+    tab <- table(data())
+    
+    if(input$order == T){
+      tab <- sort(tab)
+    }
+    
+    if(input$rev == T){
+      tab <- rev(tab)
+    }
+    return(tab)
+  })
+    
+    
+  output$table <- renderTable({
+    
+    return(tab())
+    })
+  
+  output$Bar.1 <- renderPlot({
+    req(input$colour)
+    barplot(tab(), main = "Data", col = input$colour)
+  })
 }
+
+shinyApp(ui,server)
