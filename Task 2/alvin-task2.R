8233b95b5eec2d313b41c1b9f58ba39e13c2863e
Alvin C. Nataputra <70255413+apricitea@users.noreply.github.com>
2022-02-12 01:35:50 +0700

    Add files via upload

---
+++ Task 2/alvin-task2.R
@@ -0,0 +1,88 @@
+if (interactive()) {
+
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Eksplorasi Data"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("txtFile", "Please input the data",
+                buttonLabel = "Browse...", 
+                placeholder = "No file selected"),
+      checkboxInput("withColumn", label="First row is header"),
+      radioButtons("sep", "Select separator:",
+                   c("Semicolon (;)"=";",
+                     "Dot (.)"=".",
+                     "Comma (,)"=",",
+                     "Space ( )"=" ")),
+      selectInput("variable","Select Variable",choices=""),
+      imageOutput("logo")
+    ),
+    mainPanel(
+      tableOutput("head"),
+      plotOutput("histplot"),
+      plotOutput("boxplot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$logo <- renderImage({
+    return(list(
+      src = "task2_logo.png",
+      contentType = "image/png",
+      width="30%", 
+      alt = "Logo"))
+  },deleteFile = FALSE)
+  
+  readTxtFile <- reactive({
+    if (is.null(input$txtFile))
+      return(NULL)
+    
+    df <- read.csv(input$txtFile$datapath, sep = input$sep, 
+                   header=input$withColumn)
+    return(df)
+  })
+  
+  observe({
+  updateSelectInput(session=session, 
+                    inputId="variable", 
+                    label="Select variable: ",
+                    choices = colnames(readTxtFile()))
+  })
+  
+  output$head <- renderTable({
+    if (is.null(input$txtFile))
+      return(NULL)
+    
+    data <- readTxtFile()
+    head(data)
+  })
+  
+  output$histplot <- renderPlot({
+    if (is.null(input$txtFile))
+      return(NULL)
+    
+    data      <- readTxtFile()
+    column    <- input$variable
+    hist(x=as.numeric(data[,column]), col="gold", border="black", breaks=10,
+         main=paste("Histogram of",input$variable),
+         xlab=input$variable)
+  })
+  
+  output$boxplot <- renderPlot({
+    if (is.null(input$txtFile))
+      return(NULL)
+    
+    data <- readTxtFile()
+    column <- input$variable
+    boxplot(x=as.numeric(data[,column]), 
+            main=paste("Boxplot of",input$variable),
+            xlab=input$variable)
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+}
+
