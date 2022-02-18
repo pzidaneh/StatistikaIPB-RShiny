50bb4d2510b967ca5d701f0b203c8d6556aa7f45
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2022-02-19 02:10:56 +0700

    Update Tony_Simple Regression.R

--- Task 2/Tony_Simple Regression.R
+++ Task 2/Tony_Simple Regression.R
@@ -1,67 +1,124 @@
 library(shiny)
 library(shinydashboard)
+library(colourpicker)
 
-ui <- fluidPage(
-  responsive = T,
-  titlePanel("Regresi Linear", windowTitle = "Regresi"),
+ui <- dashboardPage(skin = "blue",
+  dashboardHeader(title = "Regression"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Data", tabName = "data", icon = icon("file")),
+      menuItem("Regression", tabName = "regres", icon = icon("bar-chart-o")),
+      menuItem("Assumption", tabName = "asumsi", icon = icon("list-alt"))
+    )
+  ),
   
-  sidebarLayout(
-    sidebarPanel(
-      #Buat input berupa file
-      fileInput(inputId = "file", 
-                label = "Masukkan Data",
-                placeholder = "data.csv"),
+  
+  dashboardBody(
+    tabItems(
+      tabItem("data",
+              fluidPage(
+                responsive = T,
+                titlePanel("Data & Summary"),
+                
+                sidebarLayout(
+                  sidebarPanel(
+                    #Buat input berupa file
+                    fileInput(inputId = "file", 
+                              label = "Masukkan Data",
+                              placeholder = "data.csv"),
+                    
+                    #Buat Checkbox untuk memilih apakah akan menggunakan 
+                    checkboxInput(inputId = "chk",
+                                  label = "Baris Pertama Nama Kolom",
+                                  value = F),
+                    
+                    #Buat Select input buat Separator
+                    selectInput(inputId = "sep",
+                                label = "Pilih Pemisah pada dataset",
+                                choices = c("Comma (,)" = ",",
+                                            "Semicolon (;)" =";",
+                                            "Tab" = "\t",
+                                            "Pipe" = "|",
+                                            "Spasi" = " "),
+                                selected = ","),
+                    #Buat Pilihan Variabel X
+                    selectInput(inputId = "x",
+                                label = "Pilih Variable X",
+                                choices = NULL),
+                    
+                    #Buat Pilihan Variabel Y
+                    selectInput(inputId = "y",
+                                label = "Pilih Variable Y",
+                                choices = NULL)
+                    
+                  ),
+                  
+                  mainPanel(
+                    verbatimTextOutput(outputId = "head"),
+                    verbatimTextOutput(outputId = "txt.sum")
+                  )
+                )
+              )
+        
+      ),
       
-      #Buat Checkbox untuk memilih apakah akan menggunakan 
-      checkboxInput(inputId = "chk",
-                    label = "Baris Pertama Nama Kolom",
-                    value = F),
+      tabItem("regres",
+              fluidPage(
+                responsive = T,
+                titlePanel("Regression Plot"),
+                
+                sidebarLayout(
+                  sidebarPanel(
+                    selectInput("width",
+                                label = "Line Width",
+                                choices = c(1,2,3,4,5),
+                                selected = 1),
+                    
+                    #Buat Input warna
+                    colourpicker::colourInput("colour", label = "Pick A Colour",
+                                              value = "black", showColour = "both",
+                                              palette = "square", returnName = T,
+                                              allowTransparent = T)
+                    
+                    
+                  ),
+                  
+                  mainPanel(
+                    plotOutput(outputId = "regline")
+                  )
+                )
+              )
+      ),
       
-      #Buat Select input buat Separator
-      selectInput(inputId = "sep",
-                  label = "Pilih Pemisah pada dataset",
-                  choices = c("Comma (,)" = ",",
-                              "Semicolon (;)" =";",
-                              "Tab" = "\t",
-                              "Pipe" = "|",
-                              "Spasi" = " "),
-                  selected = ","),
-      #Buat Pilihan Variabel X
-      selectInput(inputId = "x",
-                  label = "Pilih Variable X",
-                  choices = NULL),
-      
-      #Buat Pilihan Variabel Y
-      selectInput(inputId = "y",
-                  label = "Pilih Variable Y",
-                  choices = NULL)
-      
-      
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Summary",verbatimTextOutput(outputId = "txt.sum")),
-        tabPanel("Plot Regresi", plotOutput(outputId = "regline")),
-        tabPanel("Residuals",plotOutput(outputId = "resid")),
-        tabPanel("Assumption",
-                 
-                 h3("Uji Asumsi Normalitas"),
-                 verbatimTextOutput(outputId = "norm"),
-                 
-                 h3("Uji Asumsi Heteroskedastisitas"),
-                 verbatimTextOutput(outputId = "hetero"),
-                 
-                 h3("Uji Asumsi Autokorelasi"),
-                 verbatimTextOutput(outputId = "auto"))
-      
-        )
+      tabItem("asumsi",
+              fluidPage(
+                responsive = T,
+                titlePanel("Assumption & Residuals"),
+                
+                sidebarLayout(
+                  sidebarPanel=NULL,
+                  
+                  mainPanel(
+                    plotOutput(outputId = "resid"),
+                    
+                    h3("Uji Asumsi Normalitas"),
+                    verbatimTextOutput(outputId = "norm"),
+                    
+                    h3("Uji Asumsi Heteroskedastisitas"),
+                    verbatimTextOutput(outputId = "hetero"),
+                    
+                    h3("Uji Asumsi Autokorelasi"),
+                    verbatimTextOutput(outputId = "auto")
+                  )
+                )
+              )
       )
+    )
   )
+  
 )
 
-
-server <- function(input,output,session) {
+server <- function(input,output,session){
   data <- reactive({
     req(input$file)
     file1 <- input$file
@@ -84,7 +141,7 @@ server <- function(input,output,session) {
                       choices = colnames(data()))
   })
   
-  output$txt.data <- renderPrint({
+  output$head <- renderPrint({
     print(head(data()))
   })
   
@@ -108,7 +165,8 @@ server <- function(input,output,session) {
     y <- as.numeric(data()[[input$y]])
     
     plot(x, y, pch = 16, col = "blue", xlab = paste(input$x), 
-                  ylab = paste(input$y)) + abline(reg(), col = "red")
+         ylab = paste(input$y)) +
+    abline(reg(), col = input$colour, lwd = input$width)
   })
   
   output$regline <- renderPlot(final())
